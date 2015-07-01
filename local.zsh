5617a1499d0e4bdcd475592207f3254c187be8d4
Maximiliano Bertacchini <maxiberta@gmail.com>
2015-07-01 00:58:25 -0300

    removed some unused plugins and aliases

--- local.zsh
+++ local.zsh
@@ -1,10 +1,8 @@
 # Which plugins would you like to load? (plugins can be found in ~/.oh-my-zsh/plugins/*)
-plugins=(common-aliases git-prompt jira nyan virtualenv virtualenvwrapper nvm npm python zsh_reload)
-# FIXME Manually load all of the plugins that were defined above
+plugins=(common-aliases jira nyan virtualenv virtualenvwrapper python zsh_reload)
+## FIXME Manually load all of the plugins that were defined above
 for plugin ($plugins); do
-  if [ -f $ZSH_CUSTOM/plugins/$plugin/$plugin.plugin.zsh ]; then
-    source $ZSH_CUSTOM/plugins/$plugin/$plugin.plugin.zsh
-  elif [ -f $ZSH/plugins/$plugin/$plugin.plugin.zsh ]; then
+  if [ -f $ZSH/plugins/$plugin/$plugin.plugin.zsh ]; then
     source $ZSH/plugins/$plugin/$plugin.plugin.zsh
   fi  
 done
@@ -30,7 +28,8 @@ setopt append_history
 unsetopt share_history # share command history data
 setopt no_auto_remove_slash
 
-alias rm='rm -i'
-alias cp='cp -i'
-alias mv='mv -i'
+# These aliases are already declared in common-aliases plugin
+#alias rm='rm -i'
+#alias cp='cp -i'
+#alias mv='mv -i'
 alias mc='mc -S modarin256'
