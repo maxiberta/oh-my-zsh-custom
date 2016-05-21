8c7ce6e87bce78af5c535b54134fedd01541ac83
Maximiliano Bertacchini <maxiberta@gmail.com>
2016-05-20 21:13:36 -0300

    update for ubuntu 16.04

--- local.zsh
+++ local.zsh
@@ -1,14 +1,3 @@
-# Which plugins would you like to load? (plugins can be found in ~/.oh-my-zsh/plugins/*)
-plugins=(common-aliases jira nyan virtualenv virtualenvwrapper python zsh_reload history-substring-search        zsh-syntax-highlighting)
-## FIXME Manually load all of the plugins that were defined above
-for plugin ($plugins); do
-  if [ -f $ZSH_CUSTOM/plugins/$plugin/$plugin.plugin.zsh ]; then
-    source $ZSH_CUSTOM/plugins/$plugin/$plugin.plugin.zsh
-  elif [ -f $ZSH/plugins/$plugin/$plugin.plugin.zsh ]; then
-    source $ZSH/plugins/$plugin/$plugin.plugin.zsh
-  fi
-done
-
 ZSH_THEME="agnoster"
 CASE_SENSITIVE="true"
 DISABLE_UNTRACKED_FILES_DIRTY="true"
@@ -16,9 +5,12 @@ DEFAULT_USER=max
 JIRA_URL=
 REPORTTIME=5
 LESS="FRSX"
+WORKON_HOME=${HOME}/.virtualenvs
 
 # git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 ZSH_HIGHLIGHT_HIGHLIGHTERS=(main brackets pattern root line)
+
+export TERM="xterm-256color"
 
 export STANDARD_CACHE_DIR="${XDG_CACHE_HOME:-${HOME}/.cache}/pip"
 export WHEELHOUSE="${STANDARD_CACHE_DIR}/wheelhouse"
@@ -38,3 +30,14 @@ setopt no_auto_remove_slash
 #alias cp='cp -i'
 #alias mv='mv -i'
 alias mc='mc -S modarin256'
+
+# Which plugins would you like to load? (plugins can be found in ~/.oh-my-zsh/plugins/*)
+plugins=(common-aliases jira nyan virtualenv virtualenvwrapper python zsh_reload history-substring-search) # zsh-syntax-highlighting)
+## FIXME Manually load all of the plugins that were defined above
+for plugin ($plugins); do
+  if [ -f $ZSH_CUSTOM/plugins/$plugin/$plugin.plugin.zsh ]; then
+    source $ZSH_CUSTOM/plugins/$plugin/$plugin.plugin.zsh
+  elif [ -f $ZSH/plugins/$plugin/$plugin.plugin.zsh ]; then
+    source $ZSH/plugins/$plugin/$plugin.plugin.zsh
+  fi
+done
